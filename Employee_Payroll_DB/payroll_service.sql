546d2161f21577d3448839a4d9e1c74e9f30a772
balu-pinisety <manikantapandu225@gmail.com>
2021-07-24 00:53:31 +0530

    Added: Employee payroll Table in database

--- Employee_Payroll_DB/payroll_service.sql
+++ Employee_Payroll_DB/payroll_service.sql
@@ -2,4 +2,14 @@
 create database payroll_service; #Creating Data base
 show databases; #Displays Data bases
 use payroll_service; 
-select database(); #Selects required Database
+
+#UC2
+CREATE TABLE employee_payroll ( #creates Table
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150),
+    salary DOUBLE NOT NULL,
+    start DATE NOT NULL,
+    PRIMARY KEY (id) #Primary Key To make Id unique
+);
+select * from employee_payroll;
+#drop table employee_payroll; #To delete the table
