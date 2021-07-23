8eff14da88289ab9616899e3034321f12bab07b6
balu-pinisety <manikantapandu225@gmail.com>
2021-07-24 01:02:47 +0530

    Added: Data into table of database

--- Employee_Payroll_DB/payroll_service.sql
+++ Employee_Payroll_DB/payroll_service.sql
@@ -13,3 +13,12 @@ CREATE TABLE employee_payroll ( #creates Table
 );
 select * from employee_payroll;
 #drop table employee_payroll; #To delete the table
+
+#UC3
+insert into employee_payroll(name,salary,start) values
+('KAJAL',4500.0,'2021-02-16'),
+('SATYENDRA',5500.0,'2021-04-02'),
+('SHEETHAL',7500.0,'2021-05-20'),
+('DILEEP',8750.0,'2021-01-17'),
+('AAYUSHI',6280.0,'2021-03-07'),
+('SIVA',2540.0,'2021-06-08');
