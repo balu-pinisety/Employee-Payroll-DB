3fd25e86e227f204e57ebad2c2bc5b1099bfc1b3
balu-pinisety <manikantapandu225@gmail.com>
2021-07-24 01:13:15 +0530

    Added: To retrive Data that added to database

--- Employee_Payroll_DB/payroll_service.sql
+++ Employee_Payroll_DB/payroll_service.sql
@@ -15,6 +15,7 @@ select * from employee_payroll;
 #drop table employee_payroll; #To delete the table
 
 #UC3
+#Add data to Table of database
 insert into employee_payroll(name,salary,start) values
 ('KAJAL',4500.0,'2021-02-16'),
 ('SATYENDRA',5500.0,'2021-04-02'),
@@ -22,3 +23,7 @@ insert into employee_payroll(name,salary,start) values
 ('DILEEP',8750.0,'2021-01-17'),
 ('AAYUSHI',6280.0,'2021-03-07'),
 ('SIVA',2540.0,'2021-06-08');
+
+#UC4
+#To retrieve all data added to Database
+select * from employee_payroll;
